7a8db03c2ab1e0027073f50b025dbd231f896672
jiang_20 <1499209149@qq.com>
2019-11-22 17:33:16 +0800

    qqqqqqq

---
+++ .vscode/bst.c
@@ -0,0 +1,12 @@
+
+struct node *newNode(int n)
+{
+
+}
+
+struct node *insert(struct node *root,int n)
+{
+
+}
+
+void inOrder ()
---
+++ .vscode/hui.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"bst.h"
+int main()
+{
+    struct node *root=NULL;
+    for (int i = 0; i < n; i++)
+    {
+        root=insert(root,5);
+    }
+
+    
+    
+    printf("Hello world!\n");
+    return 0;
+}
